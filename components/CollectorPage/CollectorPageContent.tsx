c29dcf888292d7fa9df3c9a599c040707b1107c6
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-16 11:05:12 -0300

    I init <MadeBySweets /> component.

--- components/CollectorPage/CollectorPageContent.tsx
+++ components/CollectorPage/CollectorPageContent.tsx
@@ -1,3 +1,4 @@
+import MadeBySweets from '../MadeBySweets';
 import CollectorPageHeader from './CollectorPageHeader';
 import Results from './Results';
 
@@ -5,13 +6,7 @@ const CollectorPageContent = () => (
   <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center sm:gap-8 md:px-6">
     <CollectorPageHeader />
     <Results />
-    <a
-      href="https://github.com/SweetmanTech/viem-transfers"
-      target="_blank"
-      className="flex justify-center items-center gap-5"
-    >
-      made with <img src="/images/zorb.png" width="50" /> by sweetman
-    </a>
+    <MadeBySweets />
   </div>
 );
 
