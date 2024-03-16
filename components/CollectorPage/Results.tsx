49b50eee73b60e5eb7b4bcd75fc998d237d931f2
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-16 19:41:51 -0300

    I replace 0x addresses with live collection data on web interface.

--- components/CollectorPage/Results.tsx
+++ components/CollectorPage/Results.tsx
@@ -1,17 +1,16 @@
+import TableRow from './TableRow';
+
 const Results = ({ snapshot }: any) => (
   <table tw="w-[75vw] w-full flex flex-col">
     <thead>
       <tr tw="flex w-full justify-between">
         <th>Collection</th>
-        <th>Number of Editions</th>
+        <th className="w-[77px]">Number of Editions</th>
       </tr>
     </thead>
     <tbody tw="flex flex-col w-full justify-between">
       {(snapshot as any).map((item: any, index: number) => (
-        <tr key={index} tw="flex justify-between">
-          <td>{item.address}</td>
-          <td>{item.numberOfEditions}</td>
-        </tr>
+        <TableRow item={item} key={index} />
       ))}
     </tbody>
   </table>
